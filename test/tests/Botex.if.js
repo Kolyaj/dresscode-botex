b7891b762ea31af3ac06bf0b3cbf60b25acc5809
kolyaj <kolyaj@yandex.ru>
2019-06-11 15:27:05 +0300

    Поменял интерфейс Botex.if

--- test/tests/Botex.if.js
+++ test/tests/Botex.if.js
@@ -3,11 +3,31 @@
 
     describe('Botex.if', function() {
         it('Botex.if-then', function() {
-            chai.assert.equal(new Botex.Template({content: Botex.if(true, {then: 'then', else: 'else'})}), 'then');
+            var result = new Botex.Template({
+                content: Botex.if(true, {
+                    then: function() {
+                        return 'then';
+                    },
+                    else: function() {
+                        return 'else';
+                    }
+                })
+            });
+            chai.assert.equal(result, 'then');
         });
 
         it('Botex.if-else', function() {
-            chai.assert.equal(new Botex.Template({content: Botex.if(false, {then: 'then', else: 'else'})}), 'else');
+            var result = new Botex.Template({
+                content: Botex.if(false, {
+                    then: function() {
+                        return 'then';
+                    },
+                    else: function() {
+                        return 'else';
+                    }
+                })
+            });
+            chai.assert.equal(result, 'else');
         });
     });
 })();
