890d85b1105996cecc780aac1113ec261e4418df
kolyaj <kolyaj@yandex.ru>
2019-09-17 12:01:19 +0300

    Botex.if теперь понимает кванты в качестве условия + Botex.ifNot

--- test/tests/Botex.if.js
+++ test/tests/Botex.if.js
@@ -29,5 +29,50 @@
             });
             chai.assert.equal(result, 'else');
         });
+
+        it('Botex.if quant', function() {
+            var q = new Quantum.Quant(true);
+            var Widget = Bricks.inherit(Botex.Widget, {
+                _render: function() {
+                    return {
+                        attrs: {
+                            title: Botex.if(q, {
+                                then: function() {
+                                    return 'then';
+                                },
+                                else: function() {
+                                    return 'else';
+                                }
+                            })
+                        }
+                    }
+                }
+            });
+            var widget = new Widget();
+            widget.mount(document.body);
+            chai.assert.equal(widget.getEl().getValue().getAttribute('title'), 'then');
+            q.setValue(false);
+            chai.assert.equal(widget.getEl().getValue().getAttribute('title'), 'else');
+        });
+
+        it('Botex.ifNot quant', function() {
+            var q = new Quantum.Quant(true);
+            var Widget = Bricks.inherit(Botex.Widget, {
+                _render: function() {
+                    return {
+                        attrs: {
+                            title: Botex.ifNot(q, function() {
+                                return 'ok';
+                            })
+                        }
+                    }
+                }
+            });
+            var widget = new Widget();
+            widget.mount(document.body);
+            chai.assert.equal(widget.getEl().getValue().getAttribute('title'), 'undefined');
+            q.setValue(false);
+            chai.assert.equal(widget.getEl().getValue().getAttribute('title'), 'ok');
+        });
     });
 })();
