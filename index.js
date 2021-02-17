09987df54ea448494d24753c781e6874250f3782
kolyaj <kolyaj@yandex.ru>
2021-02-17 13:49:58 +0300

    Давал одинаковые имена виртуальным файлам при компиляции CSS. Содержимое файлов кэшировалось, получалось плохо при компиляции одним экземпляром DressCode нескольких файлов из одной директориии.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var path = require('path');
 
 exports.compileCSS = async function(dresscode, fname) {
     fname = path.resolve(fname);
-    var dirname = path.dirname(fname);
     var basename = path.basename(fname);
     var code = `
             //#include ${basename}::
@@ -10,6 +9,6 @@ exports.compileCSS = async function(dresscode, fname) {
             return typeof Botex !== 'undefined' && Botex.CSS && Botex.CSS.compile() || '';
             //#endlayer
         `;
-    var compiledCode = await dresscode.compileCode(path.join(dirname, '_'), code, {}, [], 'css');
+    var compiledCode = await dresscode.compileCode(`${fname}.css`, code, {}, [], 'css');
     return new Function('module, exports', compiledCode)({}, {});
 };
